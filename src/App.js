cc228cfa2debc2bcebdb00d47afd2579249151d7
Josh Blumberg <jlblumberg@gmail.com>
2020-06-09 12:38:13 +0100

    Add form and result to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './App.module.css';
+import Form from './components/Form/Form';
+import Result from './components/Result/Result';
+
 
 function App() {
   return (
     <div className={styles.container}>
-      This is the parent component
+      <Form/>
+      <Result/>
     </div>
   );
 }
