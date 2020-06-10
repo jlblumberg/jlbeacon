76ce7286673ac15ac1dc26ed33c8cf22729aa425
Josh Blumberg <jlblumberg@gmail.com>
2020-06-10 13:19:24 +0100

    Add results prompt

--- src/__tests__/Result.test.js
+++ src/__tests__/Result.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Result from '../components/Result/Result';
 import { shallow } from 'enzyme';
+import mockResponse from '../__mocks__/mockResponse.json'
 
 describe('Result', () => {
 
   it('renders correctly', () => {
-    const wrapper = shallow(<Result/>);
+    const wrapper = shallow(<Result data={{ "distance": '' }}/>);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('shows a helpful prompt while awaiting a request', () => {
+    const wrapper = shallow(<Result data={{ "distance": '' }}/>)
+    expect(wrapper.find('#awaiting-request').text()).toEqual('Please fill in the form and press submit')
+  });
+
+  it('shows actual results when a request response is received', () => {
+    const wrapper = shallow(<Result data={mockResponse} />)
+    expect(wrapper.find('#results').text()).not.toContain('Please fill in the form and press submit')
+  });
+  
 
 });
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Result = ({ data }) => {
+
+  if (data.distance === "") {
+    return (
+      <div id='awaiting-request'>Please fill in the form and press submit</div>
+    )
+  }
+  
   const { distance, departure, arrival, duration, speed } = data
 
   return (
-    <div>
-      {console.log(data)}
+    <div id='results'>
+      {distance}
     </div>
   )
 }
