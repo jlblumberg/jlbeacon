cf723e51fe0154341219e80ad725b970b6d58447
Josh Blumberg <jlblumberg@gmail.com>
2020-06-10 10:28:50 +0100

    Update API request to use CORS proxy, and move handleSubmit to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import styles from './App.module.css';
 import Form from './components/Form/Form';
 import Result from './components/Result/Result';
-
+import { useState } from 'react';
+import { fetchData } from './api/index';
 
 function App() {
+
+  const [routeResults, setRouteResults] = useState({
+      distance: '',
+      departure: '',
+      arrival: '',
+      duration: '',
+      speed: ''
+  });
+
+  const handleSubmit = async (requestDetails) => {
+    const { distance, departure, arrival, duration, speed } = await fetchData(requestDetails);
+    setRouteResults({ distance, departure, arrival, duration, speed });
+    console.log("route state: ", routeResults);
+  }
+
   return (
     <div className={styles.container}>
-      <Form/>
-      <Result/>
+      <Form handleSubmit={handleSubmit}/>
+      <Result data={routeResults}/>
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const config = { headers: { 'x-api-key': process.env.REACT_APP_SEA_API_KEY } };
+const config = { headers: { 'x-api-key': process.env.REACT_APP_SEA_API_KEY, 'X-Requested-With': 'XMLHttpRequest' } };
 const startLong = '9.965629577636719'
 const startLat = '53.53296196255539'
 const endLong = '0.45069694519042963'
 const endLat = '51.503039451809734'
-const url = `https://api.searoutes.com/route/v2/sea/${startLong},${startLat};${endLong},${endLat}`
+const url = `https://cors-anywhere.herokuapp.com/https://api.searoutes.com/route/v2/sea/${startLong},${startLat};${endLong},${endLat}`
 
 export const fetchData = async () => {
   try {
     const response = await axios.get(url, config)
-    const { distance, departure, arrival, duration, speed } = response.features[0].properties
+    const { distance, departure, arrival, duration, speed } = response.data.features[0].properties
     return { distance, departure, arrival, duration, speed }
   } catch (error) {
     return new Error(error)
