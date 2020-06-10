a9833fec9e5f51ff8107fd6286135b78e625b1db
Josh Blumberg <jlblumberg@gmail.com>
2020-06-10 15:29:48 +0100

    Add formatUnixTime function

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,3 +1,12 @@
 export const metersToKms = (meters) => {
   return meters/1000
 }
+
+export const formatUnixTime = (unixTime) => {
+  const date = new Date(unixTime * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
+
+  return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+}
--- src/__tests__/Helpers.test.js
+++ src/__tests__/Helpers.test.js
@@ -1,9 +1,13 @@
-import { metersToKms } from '../Helpers.js'
+import { metersToKms, formatUnixTime } from '../Helpers.js'
 
 describe('Helper functions', () => {
 
   it('metersToKms converts meters to kms', () => {
     expect(metersToKms(1000)).toEqual(1);
+  });
+
+  it('formatUnixTime converts unix time to a readable time', () => {
+    expect(formatUnixTime(1549312452)).toEqual("20:34:12");
   });
 
 });
